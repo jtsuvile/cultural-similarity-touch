c3e9cf45b8a10988745d361a42b445fc0d8d84a3
Suvilehto Juulia <jtsuvile@t31400-la002.org.aalto.fi>
2018-07-19 12:43:11 +0100

    removed old version of tmaps

--- analyses_and_visualisations/tmaps_tworows.m
+++ analyses_and_visualisations/tmaps_tworows.m
@@ -5,12 +5,7 @@ full_labels = {'Partner' 'kid' 'Mother' 'Father' 'Sister' 'Brother' ...
     'Aunt' 'Uncle' 'Cousin' 'Cousin' ...
     'Friend' 'Friend' 'Acq.' 'Acq.' 'm kid' 'f kid' ...
      'Stranger' 'Stranger' 'rand f kid' 'rand m kid', 'check'};
-% full_labels = {'Puoliso' 'kid' '?iti' 'Is?' 'Sisko' 'Veli' ...
-%     'T?ti' 'Set?\nEno' 'Serkku' 'Serkku' ...
-%     'Yst?v?' 'Yst?v?' 'Tuttu' 'Tuttu' 'm kid' 'f kid' ...
-%      'Vieras' 'Vieras' 'rand f kid' 'rand m kid', 'check'};
 order_trim = [1 3 4 5 6 7 8 9 10 11 12 13 14 17 18];
-
 
 bodyspmdir=('/Users/jtsuvile/Documents/projects/Touch/data/BodySPM/');
 front = double(imread(sprintf('%smask_front_new.png',bodyspmdir)));
@@ -23,7 +18,7 @@ mask(mask == 1) = -1;
 outline = [front_outline back_outline];
 outline(outline<50) = 0; 
 %%
-who = 3;
+who = 1;
 
 switch who
     case 1 % z score comparisons!
@@ -40,65 +35,40 @@ switch who
         minval = -10;
     case 2 % JP prop acceptable
         load /Volumes/SCRsocbrain/japan_soctouch/data/jp/mat-files/all_proportion_acceptable.mat
-        %load /Volumes/SCRsocbrain/japan_soctouch/data/jp/mat-files/FDR_limits.mat
         cmap = colormap(hot);
         tdata = all_prop_acceptable;
         maxval = 1; 
         minval = 0;
+        cutoff_prop = 0;
     case 3 % UK scaled
         load /Volumes/SCRsocbrain/japan_soctouch/data/en/mat-files/all_proportion_acceptable.mat
-        %load /Volumes/SCRsocbrain/japan_soctouch/data/en/mat-files/FDR_limits.mat
         cmap = colormap(hot);
         tdata = all_prop_acceptable;
         maxval = 1; %0.03;
         minval = 0;
+        cutoff_prop = 0;
 end
-%%
-% load /Volumes/amor/scratch/socbrain/soctouch/word_data_all/t_sorted_all.mat
-% largest_tval = max(max(max(tdata(:,:,trim))));
-% tdata= tdata./largest_tval;
-% %  if no thresholding!
-% FDRres(:,:) = 0;
-% %% if difference between countries
-% for i=1:20
-%     tmp = tdata(:,:,i);
-%     tmp(tmp < FDRres(i,2)) = 0;
-%     tdata(:,:,i) = tmp;
-% end
-% jpdata = tdata/largest_tval;
-% tdata2 = jpdata(:,:,1:20) - endata;
+%% draw figure
 
-%%
+% set figure properties
 close all;
-cutoff_prop = 0.5;
-numcols = 15; %ceil(max(max(max(tdata))))-5;
-
+numcols = 15; 
 rows = 1;
 cols = 15;
-% diff of tvalues 
-% 
-
-
 fi = figure('Position', [100 100 800 650]);
+half = 171;
+
+%top row
 a = axes('Position', [0.1300    0.5838    0.7750    0.3412], 'xticklabel',[], 'xtick',[],  'yticklabel',[], 'ytick',[], 'Visible','off');
 tmppos = get(a,'position');
-%tmppos
 l1 = tmppos(1); 
 b1 = tmppos(2); 
 w1 = tmppos(3); 
 h1 = tmppos(4); 
-
-%delete(a);
 tmphorz = (w1/cols);
 tmpvert = (h1/rows);
 
-%cmap = colormap(jet(numcols)); 
-%cmap = [0 0 0;cmap];
-%cmap = flipud(cbrewer('div','RdBu',21));
-
-half = 171;
-
-
+% draw
 for s=1:length(order_trim)
     nth_row = (ceil(s/cols));
     nth_col = s-(nth_row-1)*cols;
@@ -108,14 +78,12 @@ for s=1:length(order_trim)
     hold on;
     target = order_trim(s);
     temp=tdata(:,1:half,target);
-    if(who==8||who==9)
+    if(who==2||who==3)
         temp(abs(temp)<cutoff_prop)=0;
-        %temp = temp;
     else
         temp(abs(temp)<FDRres(target,2))=0;
     end
     temp1 = temp - outline(:,1:half);
-    %h = imshow(temp1,[0 1]);
     h = imshow(temp1,[minval maxval]);
     set(h, 'AlphaData', ~mask(:,1:half));
     colormap(cmap);
@@ -124,19 +92,17 @@ for s=1:length(order_trim)
     axis off;
 end
 
+%bottom row
 hold on;
 b2 = axes('Position', [0.1300    0.1900    0.7750    0.3412], 'xticklabel',[], 'xtick',[],  'yticklabel',[], 'ytick',[], 'Visible','off');
 tmppos = get(b2,'position');
-%tmppos
 l = tmppos(1); 
 b = tmppos(2); 
 w = tmppos(3); 
 he = tmppos(4); 
-
-%delete(a);
 tmphorz = (w/cols);
 tmpvert = (he/rows);
-
+% draw
 for s=1:length(order_trim)
     nth_row = (ceil(s/cols));
     nth_col = s-(nth_row-1)*cols;
@@ -146,18 +112,14 @@ for s=1:length(order_trim)
     hold on;
     target = order_trim(s);
     temp=tdata(:,half:end,target);
-    if(who==8||who==9)
+    if(who==2||who==3)
         temp(abs(temp)<cutoff_prop)=0;
-        %temp = temp;
     else
         temp(abs(temp)<FDRres(target,2))=0;
     end
     temp1 = temp - outline(:,half:end);
-    %h = imshow(temp1,[0 1]);
     h = imshow(temp1,[minval maxval]);
-    %h = imagesc(temp);
     set(h, 'AlphaData', ~mask(:,half:end));
-    %colormap(map);
     colormap(cmap);
     tit = sprintf(full_labels{target});
     if mod(target,2)==0
@@ -170,7 +132,7 @@ for s=1:length(order_trim)
         titcolor = 'r';
         titpos = [100 -50 1];
     end
-    if(who==7)
+    if(who==1)
         if mod(target,2)==0
             titcolor = 'r';
             titpos = [100 10 1];
@@ -192,58 +154,29 @@ end
 
 B=colorbar;
 set(B, 'Position', [l+w+.01 0.450 .01 1.05*h1],'FontSize', 12, 'FontName','Arial')
-% if(who==8||who==9)
-%     set(B,'YTickLabel',{'Taboo','Universally\newlineacceptable'}, ...
-%                'YTick', 0:1, 'TickInterpreter','tex');
-% end
 
-%%
 allhandles = findall(fi,'type','axes');
 
 text(-.025, b+2.2*he, 'Back',  'Parent', b2,'FontSize', 16, 'FontName','Arial', 'HorizontalAlignment', 'center','rotation',90)
 text(-.025, 1.5*h1,'Front', 'Parent', a, 'FontSize', 16, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
-if(who==7)
+if(who==1)
     text(l+w+.093, 0.825, '\itZ', 'Parent', b2,'FontSize', 12, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
     text(l+w+.093, 0.920, '  score', 'Parent', b2,'FontSize', 12, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
-elseif (who==8||who==9)
+elseif (who==2||who==3)
     text(l+w+.097, 0.77, 'Proportion of subjects', 'Parent', b2,'FontSize', 12, 'FontName','Arial','HorizontalAlignment', 'left', 'rotation',90)
 else
     text(l+w+.093, 0.825, '\itT', 'Parent', b2,'FontSize', 12, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
     text(l+w+.093, 0.920, '  score', 'Parent', b2,'FontSize', 12, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
 end
-
-
-% text(-.025, b+2.2*he, 'Selk?puoli',  'Parent', b2,'FontSize', 16, 'FontName','Arial', 'HorizontalAlignment', 'center','rotation',90)
-% text(-.025, 1.5*h1,'Etupuoli', 'Parent', a, 'FontSize', 16, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
-% text(l+w+.093, 0.825, '\itT', 'Parent', b2,'FontSize', 12, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
-% text(l+w+.093, 0.920, ' -arvo', 'Parent', b2,'FontSize', 12, 'FontName','Arial','HorizontalAlignment', 'center', 'rotation',90)
-
-%notouch = text(-.02, 0.02, sprintf('Taboo\nzone'), 'Parent', a, 'Color', 'k','FontSize', 12, 'FontName','Arial', 'HorizontalAlignment', 'center');
-
-%notouch = text(-.02, 0.02, sprintf('Taboo\nzone'), 'Parent', a, 'Color', 'k','FontSize', 12, 'FontName','Arial', 'HorizontalAlignment', 'center');
-
-%bigfi = imresize(fi,5);
-%imshow(bigfi)
-%set(fi ); 
 %%
 switch who
-    case 1 % all
-        export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-JP' -png -m3.8
-    case 2 % UK
-        export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-UK' -png -m3.8
-    case 3 %girls
-        export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-UK-male-scaled' -png -m3.8
-    case 4 % boys
-        export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-UK-female-scaled' -png -m3.8
-    case 5 %girls
-        export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-JP-male-scaled' -png -m3.8
-    case 6 % boys
-        export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-JP-female-scaled' -png -m3.8
-    case 7 % z score comparison
+    case 1 % z score comparison
+        print('Z score thresholds for drawn individuals: ');
+        print(FDRres(order_trim));
         export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-tvalues-jp-minus-en' -png -m3.8
-    case 8 % all
+    case 2 % JP
         export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-JP_prop' -png -m3.8
-    case 9 % UK
+    case 3 % UK
         export_fig '/Users/jtsuvile/Documents/projects/jap-touch/visualizations/heatmaps-hot-UK_prop' -png -m3.8
 end
 %%
