54242b2d968983b07121d5c274d020b735055f37
Suvilehto Juulia <jtsuvile@t31400-la002.org.aalto.fi>
2018-07-26 12:14:43 +0100

    removing hardcoded values from workflow_2

--- workflow_2_preprocess.m
+++ workflow_2_preprocess.m
@@ -1,35 +1,29 @@
 % set BodySPM directory
-bodyspmdir = './BodySPM/';
+bodyspmdir = '/m/nbe/scratch/socbrain/cultural_comparison_code_test/cultural-universalism-touch/BodySPM';
 addpath(bodyspmdir)
 %% Binarize coloring files
 % In the raw coloring data, the colored pixels can have different values 
 % depending on how long an area has been colored. The following takes the
 % raw data matrix and saves it as full binarized raw data matrix as well as 
 % toucher-specific raw data matrices (e.g. 'sister_binary_results.mat')
-people_en = {'partner' 'kid' 'mom' 'dad' 'sister' 'brother' ...
+dataloc = '/m/nbe/scratch/socbrain/cultural_comparison_code_test/data/';
+
+dataloc_uk = [dataloc '/uk/'];
+people_uk = {'partner' 'kid' 'mom' 'dad' 'sister' 'brother' ...
     'aunt' 'uncle' 'f_cousin' 'm_cousin' ...
     'f_friend' 'm_friend' ...
     'f_contact' 'm_contact' 'f_kid' 'm_kid'...
     'f_stranger' 'm_stranger' 'rand_f_kid' 'rand_m_kid'};
 
+dataloc_jp = [dataloc '/jp/'];
 people_jp = {'partner' 'kid' 'mom' 'dad' 'sister' 'brother' ...
     'aunt' 'uncle' 'f_cousin' 'm_cousin' ...
     'f_friend' 'm_friend' ...
     'f_contact' 'm_contact' 'niece' 'nephew'...
     'f_stranger' 'm_stranger' 'rand_f_kid' 'rand_m_kid'};
 
-% British data
-dataloc_en = '/m/nbe/scratch/socbrain/japan_soctouch/data/en/';
-if(exist(dataloc_en,'dir')~=7)
-    mkdir(dataloc_en);
-end
-binarize_coloring(dataloc, people_en);
-% Japanese data
-dataloc_jp = '/m/nbe/scratch/socbrain/japan_soctouch/data/jp/';
-if(exist(dataloc_jp,'dir')~=7)
-    mkdir(dataloc_jp);
-end
-binarize_coloring(dataloc, people_jp);
+binarize_coloring(dataloc_uk, people_uk);
+binarize_coloring(dataloc_jp, people_jp);
 %% Calculate touchable body area (TI) by subject and target
 % this code will create and save the following files in datadir/mat-files
 % 'area_new.mat' with variable 'sli', which has number of coloured pixels
@@ -40,12 +34,12 @@ binarize_coloring(dataloc, people_jp);
 % Japanese data
 countSLI(dataloc_jp, bodyspmdir);
 % British data
-countSLI(dataloc_en, bodyspmdir);
+countSLI(dataloc_uk, bodyspmdir);
 %% Calculate proportion coloured per pixel (for visualisation)
 % this code will create and save 
 % '/mat-files/all_proportion_acceptable.mat' with variable 'all_prop_acceptable'
 % which will have proportion colored pixels 
 % British data
-prop_colored(dataloc_en, people_en)
+prop_colored(dataloc_uk, people_uk)
 % Japanese data
 prop_colored(dataloc_jp, people_jp)
